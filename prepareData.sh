e9b437e445797eb935bfedebfa9f7e9a862b1a4b
joerg <joerg@yovisto.com>
2021-01-11 21:36:53 +0100

    Update prepareData.sh

--- prepareData.sh
+++ prepareData.sh
@@ -3,10 +3,13 @@
 
 mkdir -p data
 cd data
-wget https://github.com/openeduhub/oeh-wlo-data-dump/raw/main/data/wirlernenonline.oeh.json.gz 
+wget https://github.com/openeduhub/oeh-wlo-data-dump/raw/main/data/wirlernenonline.oeh.json.gz
+echo "Unzipping ... "
 gunzip wirlernenonline.oeh.json.gz
 cd ..
 
 # convert to csv
+echo "Converting ... "
+docker run -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/prepareData.py /data/wirlernenonline.oeh.json
 
- docker run -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/prepareData.py /data/wirlernenonline.oeh.json
+echo "Done :-) "
