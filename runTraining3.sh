4ce425b4cca86f456f6422e9a1ca97c7df518c08
joerg <joerg@yovisto.com>
2021-03-24 16:01:57 +0100

    Update runTraining3.sh

--- runTraining3.sh
+++ runTraining3.sh
@@ -1,5 +1,7 @@
 cd data
-unzip wirlernenonline.oeh3.csv.zip 
+if [ ! -f "wirlernenonline.oeh3.csv" ]; then
+	unzip wirlernenonline.oeh3.csv.zip 
+fi
 cd ..
 
 docker run --runtime=nvidia -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/training3.py /data/wirlernenonline.oeh3.csv
