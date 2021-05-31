19fc15ba6ac933f59f7073f8878f07d5ec4a594b
joerg <joerg@yovisto.com>
2021-05-31 22:18:45 +0200

    Update runTraining.sh

--- runTraining.sh
+++ runTraining.sh
@@ -4,6 +4,6 @@ if [ ! -f "wirlernenonline.oeh3.csv" ]; then
 fi
 cd ..
 
-docker run --runtime=nvidia -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/training.py /data/wirlernenonline.oeh3.csv
+docker run --runtime=nvidia -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/training4.py /data/wirlernenonline.oeh4.csv
 
 #docker run --runtime=nvidia -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/training.py /data/wirlernenonline.oeh.csv
