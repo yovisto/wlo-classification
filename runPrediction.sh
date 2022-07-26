f96c4d871158cfa538d192d7191fd2dc2373766c
yovisto <joerg@yovisto.com>
2022-07-26 10:21:07 +0200

    Update runPrediction.sh

--- runPrediction.sh
+++ runPrediction.sh
@@ -1,3 +1,3 @@
 
 
-docker run -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/predict.py /data/wirlernenonline.oeh3.h5 /data/wirlernenonline.oeh3.npy  /data/wirlernenonline.oeh3.pickle "$1"
+docker run -v `pwd`/data:/data -v `pwd`/src:/src wlo-cuda:1.0 /usr/bin/python3 /src/predict.py /data/wirlernenonline.oeh.h5 /data/wirlernenonline.oeh.npy  /data/wirlernenonline.oeh.pickle "$1"
