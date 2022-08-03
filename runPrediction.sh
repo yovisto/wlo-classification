6468eac3804e5918144e096522b6d8a1a4ff0753
yovisto <joerg@yovisto.com>
2022-08-03 09:28:47 +0200

    Update runPrediction.sh

--- runPrediction.sh
+++ runPrediction.sh
@@ -1,3 +1,3 @@
 
 
-docker run -v `pwd`/data:/data -v `pwd`/src:/src wlo-cuda:1.0 /usr/bin/python3 /src/predict.py /data/model /data/class_names.npy "$1"
+docker run -v `pwd`/data:/data -v `pwd`/src:/src wlo-cuda:1.0 /usr/bin/python3 /src/predict.py /data/model-wlo-classification "$1"
