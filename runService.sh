11dd508315287a29337d1036566a9e534b70f362
yovisto <joerg@yovisto.com>
2022-08-03 09:28:05 +0200

    Update runService.sh

--- runService.sh
+++ runService.sh
@@ -1,2 +1,2 @@
-docker run  -p 8080:8080 -d --name wlo-classify -v `pwd`/data:/data -v `pwd`/src:/scr wlo-cuda:1.0 /usr/bin/python3 /scr/webservice.py /data/wirlernenonline.oeh3.h5 /data/wirlernenonline.oeh3.npy  /data/wirlernenonline.oeh3.pickle 
+docker run  -p 8080:8080 -d --name wlo-classify -v `pwd`/data:/data -v `pwd`/src:/src wlo-cuda:2.0 /usr/bin/python3 /src/webservice.py /data/model-wlo-classification
 
